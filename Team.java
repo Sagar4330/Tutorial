243c6d777f283da3ace70ebba7aa18152ecf122e
Sagar4330 <121375833+Sagar4330@users.noreply.github.com>
2022-12-25 18:14:08 +0530

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,16 @@
+package com.jsp.ipl;
+
+public class Team 
+{
+	String name;
+	int points;
+	char []wrl=new char[5];
+	public Team(String name, int points, char[] wrl) {
+		
+		this.name = name;
+		this.points = points;
+		this.wrl = wrl;
+	}
+	
+
+}
